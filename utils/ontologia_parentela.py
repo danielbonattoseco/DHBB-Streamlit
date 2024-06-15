f9e4b3a422b981ec3d812df62be4af1552c60e49
Daniel Bonatto Seco <danielbonattoseco@hotmail.com>
2024-06-15 01:58:12 -0300

    Encoding Ontologia

--- utils/ontologia_parentela.py
+++ utils/ontologia_parentela.py
@@ -1,11 +1,11 @@
 def masculina():
-    with open('dicts/ontologia_parentela_masculina.txt', 'r') as f:
+    with open('dicts/ontologia_parentela_masculina.txt', 'r', encoding='latin-1') as f:
         masculina = [line.strip() for line in f]
         
     return masculina
         
 def feminina():
-    with open('dicts/ontologia_parentela_feminina.txt', 'r') as f:
+    with open('dicts/ontologia_parentela_feminina.txt', 'r', encoding='latin-1') as f:
         feminina = [line.strip() for line in f]
         
     return feminina
